22019de38540782efcff1244434453c106e1f1f0
pythonmustafa <115861627+pythonmustafa@users.noreply.github.com>
2026-06-24 20:08:39 +0000

    fix monorepo structure

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 1. API isteklerini ayırmak için bir ön ek ekle
+  app.setGlobalPrefix('api'); 
+
   app.enableCors({
     origin: '*',
   });
 
   await app.listen(process.env.PORT || 3000);
-  
 }
 bootstrap();
